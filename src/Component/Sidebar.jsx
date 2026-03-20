a42c267923d7a29f5c59d35c984bafd527167f5e
Rishabh kaushik <145091332+rishavkaushik70@users.noreply.github.com>
2026-03-20 16:33:33 +0530

    Change the sidebar component

--- src/Component/Sidebar.jsx
+++ src/Component/Sidebar.jsx
@@ -10,14 +10,6 @@ const Sidebar = ({ selectedTab, setSelectedTab, setLoginButton }) => {
         className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none"
       >
         {" "}
-        <svg
-          className="bi pe-none me-2"
-          width="40"
-          height="32"
-          aria-hidden="true"
-        >
-          <use xlinkHref="#bootstrap"></use>
-        </svg>{" "}
         <span className="fs-4">FriendSpot</span>{" "}
       </a>{" "}
       <hr />{" "}
